b3cf9bda92f430c4dfa63243722ed007c661db38
darekb <kontakt@3wm.pl>
2016-12-27 14:56:35 +0100

    fix server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ var defaultApp = admin.initializeApp({
       "auth_uri": "https://accounts.google.com/o/oauth2/auth",
       "token_uri": "https://accounts.google.com/o/oauth2/token",
       "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-      "client_x509_cert_url": process.env..client_x509_cert_url
+      "client_x509_cert_url": process.env.client_x509_cert_url
     }),
     databaseURL: config.firebaseUrl
 });
